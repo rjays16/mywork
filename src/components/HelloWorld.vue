08c86e9232224d096b119cccbb73e14c3f229ea3
allan81 <rjaycondiman81@gmail.com>
2022-03-31 20:44:57 +0800

    my_work

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+
+  <div id="box"></div>
+    <div id="img1">
+      <img src="../../image/ONE_trans.png">
+    </div>
+
+    <div id="img2">
+      <img src="../../image/Appreciation.png">
+    </div>
+
+    <div id="cover">
+      <img src="../../image/pexels.jpeg">
+    </div>
+
+      <div id="contain_text">
+    <div id="text">
+      <center>
+        <h1 class="for_overline">4th ANNUAL MEETING OF </h1>
+        <h1>ASIAN SOCIETY OF DERMAPATHOLOGY </h1>
+        <h1 class="for_overline">OCTOBER 24 -25 2022</h1>
+      </center>
+    </div>
+  </div>
+
+    <div id="bottom-top"></div>
+      <div id="text2">
+        <center><h1>SAVE THE DATE!</h1></center>
+        <center><p>REGISTRATION OPENS ON APRIL 18, 2022</p></center>
+      </div>
+
+    <div id="text-message">
+      <div id="content">
+         <h1>Message from the ASD President</h1>
+      </div>
+          </div>
+    </div>
+
+
+</template>
+
+<script>
+import "../../css/style.css"
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
