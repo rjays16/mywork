030bd08be966ba5b6051a114c8b3435166ffb77b
allan81 <rjaycondiman81@gmail.com>
2022-04-01 10:51:28 +0800

    for social asd meeting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -345,6 +345,13 @@
       <button>SUSCRIBE TO ASD 2022 NEWSLETTER</button>
       </div>
 
+    <div id="social">
+      <img src="../../image/social.png">
+    </div>
+
+    <div id="social_mess">
+      <h1>@ASDMeeting2022</h1>
+    </div>
     <div id="for_blue2"></div>
     <div id="for_bue2_mess"><p>2022 All Rights Reserved © Asian Society of Dermatopathology | October 24-25, 2022</p></div>
     <div id="for_yellow2"></div>
