30891aaf62889a12e96647714fb4ec53d6230d1e
allan81 <rjaycondiman81@gmail.com>
2022-04-08 17:38:27 +0800

    fees and table style

--- src/components/PromoVideo.vue
+++ src/components/PromoVideo.vue
@@ -1,12 +1,78 @@
 <template>
-<!--<div id="vid">-->
-<!--          <iframe src="https://player.vimeo.com/video/694789792?h=918ac78c36"-->
-<!--          width="740" height="564" frameborder="0" allow="autoplay; fullscreen"-->
-<!--                  allowfullscreen></iframe>-->
-<!--</div>-->
-  <div></div>
   <section>
+  <div class="container mb-5">
+    <div class="row">
+      <div class="container">
+      <div class="col-lg-12 col-md-12">
+      <img src="../../image/video-frame.png" id="content-frame">
+          <center><iframe src="https://player.vimeo.com/video/694789792?h=918ac78c36"
+           width="540" height="360" frameborder="0" allow="autoplay; fullscreen;
+          picture-in-picture"></iframe></center>
+        </div>
+        </div>
+      </div>
+    </div>
 
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-9 col-md-12 mb-5">
+          <div class="title bg-030C4C corner-rounded text-white p-3">
+            <h3 class="text-uppercase text-center rajdhani-bold ">
+              Registration fees and deadlines
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container mb-5">
+      <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-12 d-inline-block">
+          <div class="table-responsive">
+          <table class="table table-bordered">
+            <thead>
+            <tr class="text-center text-white align-middle dmsans-bold" id="fees">
+              <th>
+                <span class="text-uppercase">CATEGORIES</span>
+              </th>
+              <td>EARLY BIRD RATE <br>runs until August 15,2022</td>
+              <td>STANDARD RATE <br> will commence after August 15 to <br>September 15,2022</td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr class="text-center dmsans-regular">
+              <td class="text-start dmsans-bold">ASD Member</td>
+              <td>USD 100</td>
+              <td>USD 150</td>
+            </tr>
+            <tr class="text-center dmsans-regular">
+              <td class="text-start dmsans-bold">Non-ASD Member</td>
+              <td>USD 200</td>
+              <td>USD 250</td>
+            </tr>
+            <tr class="text-center dmsans-regular">
+              <td class="text-start dmsans-bold">Resident/Fellow</td>
+              <td>USD 50</td>
+              <td>USD 100</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+          </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-9 col-md-12 mb-5">
+          <div class="title bg-030C4C corner-rounded text-white p-3">
+            <h3 class="text-uppercase text-center rajdhani-bold ">
+              call for abstract
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
